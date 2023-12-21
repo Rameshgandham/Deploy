812adfa1f9d3df1ed586e1c3ae36b6ed98557bc2
Rameshgandham <153059628+Rameshgandham@users.noreply.github.com>
2023-12-21 10:02:53 +0530

    Add files via upload
    commited first

---
+++ src/main/java/com/deployjob/Class1.java
@@ -0,0 +1,11 @@
+package com.deployjob;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my deploy job is completed");
+
+	}
+
+}
